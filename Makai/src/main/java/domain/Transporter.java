78d1618c96d9fb9f21bb8bbf395ad135f60ec255
sanframar <raznnok@gmail.com>
2018-03-12 14:37:30 +0100

    Añadido mas populate y retoques de dominio.

--- Makai/src/main/java/domain/Transporter.java
+++ Makai/src/main/java/domain/Transporter.java
@@ -1,36 +1,52 @@
+
 package domain;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
-public class Transporter extends Actor{
-	
+public class Transporter extends Actor {
+
 	// Constructors ----------------------------------------------------------
-	public Transporter(){
+	public Transporter() {
 		super();
 	}
-	
+
+
 	// Attributes -------------------------------------------------------------
-	
-	private Double avgRating;
+
+	private Double	avgRating;
+
 
 	@NotNull
 	@Min(0)
 	public Double getAvgRating() {
-		return avgRating;
+		return this.avgRating;
 	}
 
-	public void setAvgRating(Double avgRating) {
+	public void setAvgRating(final Double avgRating) {
 		this.avgRating = avgRating;
 	}
-	
+
+
 	// Relationships ----------------------------------------------------------
-	private Travel travelPassenger;
-	
+	private Travel	travelPassenger;
+
+
+	@Valid
+	@ManyToOne(optional = true)
+	public Travel getTravelPassenger() {
+		return this.travelPassenger;
+	}
+
+	public void setTravelPassenger(final Travel travelPassenger) {
+		this.travelPassenger = travelPassenger;
+	}
 
 }
