3eec789fabb54bd34a768ff8c8bbaa138e32792e
sanframar <raznnok@gmail.com>
2018-03-12 11:29:02 +0100

    Problema con el datatype resuelto en la clase Travel.

--- Makai/src/main/java/domain/Travel.java
+++ Makai/src/main/java/domain/Travel.java
@@ -1,9 +1,13 @@
+
 package domain;
 
 import java.util.Date;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
@@ -12,93 +16,107 @@ import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
-public class Travel  extends DomainEntity {
+public class Travel extends DomainEntity {
+
 	// Constructors ----------------------------------------------------------
-	public Travel(){
+	public Travel() {
 		super();
 	}
-	
+
+
 	// Attributes -------------------------------------------------------------
-	
-	private Coordinates origin;
-	private Coordinates destination;
-	private Date startMoment;
-	private Date endMoment;
-	private Integer animalSeats;
-	private Integer humanSeats;
-	
+
+	private Coordinates	origin;
+	private Coordinates	destination;
+	private Date		startMoment;
+	private Date		endMoment;
+	private Integer		animalSeats;
+	private Integer		humanSeats;
+
+
 	@NotNull
+	@Valid
+	@AttributeOverrides({
+		@AttributeOverride(name = "country", column = @Column(name = "countryOrigin")), @AttributeOverride(name = "state", column = @Column(name = "stateOrigin")), @AttributeOverride(name = "province", column = @Column(name = "provinceOrigin")),
+		@AttributeOverride(name = "city", column = @Column(name = "cityOrigin")), @AttributeOverride(name = "zip_code", column = @Column(name = "zip_codeOrigin")),
+	})
 	public Coordinates getOrigin() {
-		return origin;
+		return this.origin;
 	}
-	public void setOrigin(Coordinates origin) {
+	public void setOrigin(final Coordinates origin) {
 		this.origin = origin;
 	}
-	
+
 	@NotNull
+	@Valid
+	@AttributeOverrides({
+		@AttributeOverride(name = "country", column = @Column(name = "countryDestination")), @AttributeOverride(name = "state", column = @Column(name = "stateDestination")),
+		@AttributeOverride(name = "province", column = @Column(name = "provinceDestination")), @AttributeOverride(name = "city", column = @Column(name = "cityDestination")),
+		@AttributeOverride(name = "zip_code", column = @Column(name = "zip_codeDestination")),
+	})
 	public Coordinates getDestination() {
-		return destination;
+		return this.destination;
 	}
-	public void setDestination(Coordinates destination) {
+	public void setDestination(final Coordinates destination) {
 		this.destination = destination;
 	}
-	
+
 	@NotNull
 	public Date getStartMoment() {
-		return startMoment;
+		return this.startMoment;
 	}
-	public void setStartMoment(Date startMoment) {
+	public void setStartMoment(final Date startMoment) {
 		this.startMoment = startMoment;
 	}
-	
+
 	@NotNull
 	public Date getEndMoment() {
-		return endMoment;
+		return this.endMoment;
 	}
-	public void setEndMoment(Date endMoment) {
+	public void setEndMoment(final Date endMoment) {
 		this.endMoment = endMoment;
 	}
-	
+
 	@Min(0)
 	public Integer getAnimalSeats() {
-		return animalSeats;
+		return this.animalSeats;
 	}
-	public void setAnimalSeats(Integer animalSeats) {
+	public void setAnimalSeats(final Integer animalSeats) {
 		this.animalSeats = animalSeats;
 	}
-	
+
 	@Min(0)
 	public Integer getHumanSeats() {
-		return humanSeats;
+		return this.humanSeats;
 	}
-	public void setHumanSeats(Integer humanSeats) {
+	public void setHumanSeats(final Integer humanSeats) {
 		this.humanSeats = humanSeats;
 	}
-	
+
+
 	// Relationships ----------------------------------------------------------
-	private Transporter transporterOwner;
-	private Vehicle vehicle;
+	private Transporter	transporterOwner;
+	private Vehicle		vehicle;
+
 
 	@Valid
 	@NotNull
 	@ManyToOne(optional = false)
 	public Transporter getTransporterOwner() {
-		return transporterOwner;
+		return this.transporterOwner;
 	}
-	public void setTransporterOwner(Transporter transporterOwner) {
+	public void setTransporterOwner(final Transporter transporterOwner) {
 		this.transporterOwner = transporterOwner;
 	}
-	
+
 	@Valid
 	@NotNull
 	@ManyToOne(optional = false)
 	public Vehicle getVehicle() {
-		return vehicle;
+		return this.vehicle;
 	}
-	public void setVehicle(Vehicle vehicle) {
+	public void setVehicle(final Vehicle vehicle) {
 		this.vehicle = vehicle;
 	}
-	
-	
 
 }
