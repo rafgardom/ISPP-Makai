8262fcc6695366a105fb7f901a49bdecb38f4cd2
sanframar <raznnok@gmail.com>
2018-03-19 21:16:54 +0100

    Modificado los servicios de Notification y Offer. Modificado el dominio de Duration.

--- Makai/src/main/java/services/OfferService.java
+++ Makai/src/main/java/services/OfferService.java
@@ -1,18 +1,23 @@
 
 package services;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 
 import repositories.OfferRepository;
 import repositories.RequestRepository;
+import domain.Coordinates;
+import domain.Duration;
 import domain.Offer;
 import domain.Request;
 import domain.Trainer;
+import forms.OfferForm;
 
 @Service
 @Transactional
@@ -146,5 +151,51 @@ public class OfferService {
 		if (!nonAcceptedOffers.isEmpty())
 			this.offerRepository.delete(nonAcceptedOffers);
 
+	}
+
+	public Offer reconstruct(final OfferForm offerForm, final BindingResult binding) throws IOException {
+		Assert.notNull(offerForm);
+		Offer result;
+
+		if (offerForm.getId() == 0)
+			if (offerForm.getRequest().getAnimal() == null)
+				result = this.createWithoutAnimal(offerForm.getRequest());
+			else
+				result = this.createWithAnimal(offerForm.getRequest());
+		else
+			result = this.findOne(offerForm.getId());
+
+		result.setDestination(new Coordinates(offerForm.getCountry(), offerForm.getCity(), offerForm.getState(), offerForm.getProvince(), offerForm.getZip_code()));
+		result.setStartMoment(offerForm.getStartMoment());
+		result.setPrice(offerForm.getPrice());
+		result.setComment(offerForm.getComment());
+		result.setDuration(new Duration(offerForm.getYear(), offerForm.getMonth(), offerForm.getWeek(), offerForm.getDay()));
+		result.setAnimal(offerForm.getAnimal());
+		result.setRequest(offerForm.getRequest());
+
+		return result;
+	}
+
+	public OfferForm toFormObject(final Offer offer) {
+		Assert.notNull(offer);
+		final OfferForm result = new OfferForm();
+
+		result.setCountry(offer.getDestination().getCountry());
+		result.setState(offer.getDestination().getState());
+		result.setProvince(offer.getDestination().getProvince());
+		result.setCity(offer.getDestination().getCity());
+		result.setZip_code(offer.getDestination().getZip_code());
+		result.setStartMoment(offer.getStartMoment());
+		result.setPrice(offer.getPrice());
+		result.setComment(offer.getComment());
+		result.setPrice(offer.getPrice());
+		result.setYear(offer.getDuration().getYear());
+		result.setMonth(offer.getDuration().getMonth());
+		result.setWeek(offer.getDuration().getWeek());
+		result.setDay(offer.getDuration().getDay());
+		result.setAnimal(offer.getAnimal());
+		result.setRequest(offer.getRequest());
+
+		return result;
 	}
 }
