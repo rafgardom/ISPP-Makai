ba8b17ec5ec2ff2a2412e8df7528cdf1a6a37257
sanframar <raznnok@gmail.com>
2018-04-08 19:53:55 +0200

    Ahora las notificaciones se orden por fecha. #92

--- Makai/src/main/java/repositories/NotificationRepository.java
+++ Makai/src/main/java/repositories/NotificationRepository.java
@@ -12,7 +12,7 @@ import domain.Notification;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
 
-	@Query("select n from Notification n join n.actor a where a.id=?1")
+	@Query("select n from Notification n join n.actor a where a.id=?1 order by n.moment DESC")
 	Collection<Notification> findByActorId(int actorId);
 
 	@Query("select count(n) from Notification n where n.actor.id=?1 and n.isRead=false")
